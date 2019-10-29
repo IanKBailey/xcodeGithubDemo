ae5f476577356a72be11bc91df17827616663adf
Ian Bailey <iankbailey2@gmail.com>
2019-10-29 11:11:38 -0400

    Initial Commit

---
+++ xcodeGithubDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xcodeGithubDemo
+//
+//  Created by Ian Bailey on 10/29/19.
+//  Copyright © 2019 Ian Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
